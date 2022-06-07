a1c256abd12529d77975381119fd483e80b2af29
lavani-tshimologong <lavani@dsa.tshimologong.joburg.com>
2022-06-07 15:24:15 +0200

    final project

---
+++ ADO6ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AccessLayer;
+namespace AD06ConsoleApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is a connection string which is used to store the details.
+            string connect = "Data Source=DESKTOP-CKH5QSL;Initial Catalog=HarvestDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            //access the function from the access layer class
+            Access.Connect(connect);
+           
+        }
+    }
+}
+
---
+++ AccessLayer/Access.cs
@@ -0,0 +1,68 @@
+﻿using System.Data.SqlClient;
+namespace AccessLayer
+{
+    public class Access
+    {
+        // method grabs string input
+        //create a method that would connect to the database
+        public static void Connect(string connectionString)
+        {
+            //create connection
+            SqlConnection con = new SqlConnection(connectionString);
+          
+
+
+
+            //create query that writes to database
+            string sql = "Insert Into Department( Dept_Name, Dept_Location) values ('Finance', 'Fourth Floor')";
+            
+            //open connection
+            con.Open();
+
+            //create a sql command referencing the connection
+            SqlCommand cmd = new SqlCommand(sql, con);
+            
+        
+
+
+            cmd.ExecuteNonQuery();
+
+            //close connection
+            con.Close();
+
+            //open connection
+            con.Open();
+            //create query that reads from database
+            string sql2 = "SELECT * FROM Department";
+            //create a sql command referencing the connection
+            SqlCommand cmd2 = new SqlCommand(sql2, con);
+
+            //create sql data reader
+            SqlDataReader dataReader = cmd2.ExecuteReader();
+
+            //while its reading
+            while (dataReader.Read())
+            {
+                
+                 //write to console
+                
+                var Dept_ID = dataReader.GetInt32(0);
+                var Dept_Name = dataReader["Dept_Name"].ToString();
+                var Location = dataReader["Dept_Location"].ToString();
+
+
+                Console.WriteLine(Dept_ID + " " + Dept_Name + " " + Location);
+
+
+            }
+            //close connection
+            con.Close();
+            //dispose of connection
+            con.Dispose();
+
+
+
+
+        }
+    }
+}
